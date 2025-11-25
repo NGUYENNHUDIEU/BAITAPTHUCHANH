ebfb21a1098c8f8202478fd8a6664e1d2983b1a2
NGUYENNHUDIEU <nguyeendieuu@gmail.com>
2025-11-25 23:50:48 +0700

    Update cau3_7.py

--- bai 3/cau3_7.py
+++ bai 3/cau3_7.py
@@ -1,6 +1,8 @@
+print('họ tên: Nguyễn Như Diệu; MSSV:245752021610124')
 def checkValue(n):  
   if n%2 == 0:  
    print ("Đây là một số chẵn")  
   else:  
    print ("Đây là một số lẻ")
 checkValue(6) 
+

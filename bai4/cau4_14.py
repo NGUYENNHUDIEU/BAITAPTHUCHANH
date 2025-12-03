52d0232da25826f7d5afd163da863f657f31a58b
NGUYENNHUDIEU <nguyeendieuu@gmail.com>
2025-12-03 07:26:55 +0700

    Add student info and reorder list input

--- bai4/cau4_14.py
+++ bai4/cau4_14.py
@@ -1,4 +1,6 @@
+print('họ tên: Nguyễn Như Diệu; MSSV:245752021610124')
 ds = input("Nhập các phần tử của list (cách nhau bằng khoảng trắng): ").split()
 print("\nList ban đầu:", ds)
 ds.sort()
 print("\nList sau khi sắp xếp:", ds)
+

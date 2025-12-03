f5bd1c8c9bc081d091b8b9cdd7c7bb197a2f24b9
NGUYENNHUDIEU <nguyeendieuu@gmail.com>
2025-12-03 07:30:37 +0700

    Update cau4_22.py

--- bai4/cau4_22.py
+++ bai4/cau4_22.py
@@ -1,3 +1,4 @@
+print('họ tên: Nguyễn Như Diệu; MSSV:245752021610124')
 def main():
     result = []
     for num in range(1000, 3001):  # từ 1000 đến 3000
@@ -8,3 +9,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+

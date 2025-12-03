efb2c9a43dd7d8c7fa9a19f1c6eb8a2e404f4d1f
NGUYENNHUDIEU <nguyeendieuu@gmail.com>
2025-12-03 07:37:02 +0700

    Refactor Circle class and add student info
    Added student information and reformatted Circle class.

--- bai6/cau6_1.py
+++ bai6/cau6_1.py
@@ -1,3 +1,4 @@
+print('họ tên: Nguyễn Như Diệu; MSSV:245752021610124')
 class Circle(object): 
 def __init__(self, r): 
 self.radius = r 
@@ -5,3 +6,4 @@ def area(self):
 return self.radius**2*3.14 
 aCircle = Circle(2) 
 print (aCircle.area())
+
